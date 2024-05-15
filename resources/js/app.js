208d5dad450cabe45d2cea9c44f593cc48287568
nikoladomazetovikj <nikolad150@gmail.com>
2024-05-15 16:33:52 +0200

    chore: inertia and vuetify setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
 import './bootstrap';
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+import {createVuetify} from "vuetify";
+import {components, directives} from "vuetify/dist/vuetify.js";
+
+createInertiaApp({
+
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+        return pages[`./Pages/${name}.vue`]
+    },
+    setup({ el, App, props, plugin }) {
+        const vuetify = createVuetify({
+            components,
+            directives,
+            theme: {
+                defaultTheme: 'dark'
+            }
+        })
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(vuetify)
+            .mount(el)
+    },
+})
