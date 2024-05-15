9f0afea5a4a502a48c80dd85298c5f1c6f01241a
nikoladomazetovikj <nikolad150@gmail.com>
2024-05-15 19:24:38 +0200

    fix: use guest middleware for auth routes

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,14 @@ Route::get('/', function () {
     return Inertia::render('Welcome');
 });
 
-Route::get('/register', [\App\Http\Controllers\Auth\AuthenticatedSessionController::class, 'create'])->name('register');
-Route::post('/register', [\App\Http\Controllers\Auth\AuthenticatedSessionController::class, 'store']);
-Route::get('/login', [\App\Http\Controllers\Auth\AuthenticatedSessionController::class, 'session'])->name('login');
-Route::post('/login', [\App\Http\Controllers\Auth\AuthenticatedSessionController::class, 'login']);
+
+Route::middleware(['guest'])->group(function () {
+    Route::get('/register', [\App\Http\Controllers\Auth\AuthenticatedSessionController::class, 'create'])->name('register');
+    Route::post('/register', [\App\Http\Controllers\Auth\AuthenticatedSessionController::class, 'store']);
+    Route::get('/login', [\App\Http\Controllers\Auth\AuthenticatedSessionController::class, 'session'])->name('login');
+    Route::post('/login', [\App\Http\Controllers\Auth\AuthenticatedSessionController::class, 'login']);
+});
+
 
 Route::get('/dashboard', function () {
     return Inertia::render('Welcome');
